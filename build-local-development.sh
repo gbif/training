24fd7c1ff3f124258262c763bc5d8d5fe8bc9521
Matthew Blissett <mblissett@gbif.org>
2025-03-06 15:51:35 +0100

    Build date in footer for non-prod sites.

--- build-local-development.sh
+++ build-local-development.sh
@@ -10,6 +10,6 @@ docker run -u $(id -u) -v $PWD:/antora:Z --rm -t -e HOME=/antora antora/antora:3
 for lang in en; do
   echo "------- Generating $lang --------"
   rm -Rf output/$lang
-  docker run -u $(id -u) -e CI=true -e env=local -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 development-$lang-playbook.yml --attribute env=local
+  docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 development-$lang-playbook.yml --attribute env=local
 done
 echo
